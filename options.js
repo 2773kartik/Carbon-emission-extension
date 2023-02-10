ae71d69ceff11044b7f1af6bddb1ab4df1041333
Kartik Tiwari <mekartik27@gmail.com>
2023-02-11 01:49:19 +0530

    initial commit

---
+++ options.js
@@ -0,0 +1,41 @@
+const displayPages = async () => {
+    const visitedPages = await PageService.getPages();
+    const pageList = document.getElementById('page-list');
+    pageList.innerHTML = '';
+    
+    visitedPages.forEach(page => {
+        const pageItem = document.createElement('li');
+        pageList.appendChild(pageItem);
+        
+        const pageLink = document.createElement('a');
+        pageLink.title = page.title;
+        pageLink.innerHTML = page.title;
+        pageLink.href = page.url;
+        pageLink.onclick = async (ev) => {
+            ev.preventDefault();
+            await chrome.tabs.create({ url: ev.target.href, active: false });
+        };
+        pageItem.appendChild(pageLink);
+
+        const pageItem2 = document.createElement('li');
+        pageList.appendChild(pageItem2);
+        const val = document.createElement('p');
+        val.title = page.memory;
+        val.innerHTML = page.memory;
+        pageItem2.appendChild(val);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await displayPages();
+
+    
+    // Clear history
+    const clearHistoryBtn = document.getElementById('clear-history');
+    clearHistoryBtn.onclick = async () => {
+        await PageService.clearPages();
+        await displayPages();
+    };
+    await acho.quiet();
+});
---
+++ popup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    const dialogBox = document.getElementById('dialog-box');
+    
+    const carb = new Carbon();
+    const tab = await carb.getActiveTab();
+    const bark = carb.getTitle(tab.title);
+
+    dialogBox.innerHTML = bark;
+    
+    // Store page.
+    
+    const mem = window.performance.memory.usedJSHeapSize;
+    await PageService.savePage(tab.title, tab.url, mem);
+});
+
+const settingsBtn = document.getElementById('optOpen');
+settingsBtn.onclick = () => {
+    chrome.runtime.openOptionsPage();
+};
